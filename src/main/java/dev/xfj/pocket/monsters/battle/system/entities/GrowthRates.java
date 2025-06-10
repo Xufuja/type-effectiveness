92dd98ef97eac645ed8659c07d4aeea9bc4c296c
Xufuja <xufuja@gmail.com>
2025-06-10 17:54:03 +0200

    Increased allowed length for formula in GrowthRates

--- src/main/java/dev/xfj/pocket/monsters/battle/system/entities/GrowthRates.java
+++ src/main/java/dev/xfj/pocket/monsters/battle/system/entities/GrowthRates.java
@@ -1,5 +1,6 @@
 package dev.xfj.pocket.monsters.battle.system.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
@@ -8,5 +9,6 @@ public class GrowthRates {
     @Id
     private Integer id;
     private String identifier;
+    @Column(length = 512)
     private String formula;
 }
